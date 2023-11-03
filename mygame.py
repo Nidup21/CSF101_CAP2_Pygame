3d1acdab07ef214b43f20e2bafc5f22a332cdbe9
Nidup Dorji <nidupdorji@pop-os.localdomain>
2023-11-03 22:38:54 +0600

    new code

--- mygame.py
+++ mygame.py
@@ -1,9 +1,8 @@
-
-
 import pygame
 import random
 import os
 
+# Initialize pygame
 pygame.init()
 
 # Screen dimensions
@@ -27,8 +26,8 @@ bird_width = 50
 bird_height = 50
 bird = pygame.transform.scale(bird, (bird_width, bird_height))
 
-knife_width = 20
-knife_height = 20
+knife_width = 30
+knife_height = 30
 knife = pygame.transform.scale(knife, (knife_width, knife_height))
 
 # Bird initial position and velocity
@@ -46,7 +45,7 @@ knife_shot = False  # Indicates whether the knife is in flight
 score = 0
 
 # Chances and Game Over
-chances = 3
+chances = 4
 game_over = False
 
 # Initialize the clock to control the frame rate
@@ -55,6 +54,15 @@ clock = pygame.time.Clock()
 # Define font for displaying text
 font = pygame.font.Font(None, 36)
 
+# Define font for the game menu
+menu_font = pygame.font.Font(None, 72)
+
+# Game state
+STATE_MENU = 0
+STATE_PLAYING = 1
+STATE_GAME_OVER = 2
+current_state = STATE_MENU  # Start with the menu
+
 # Main game loop
 running = True
 while running:
@@ -62,68 +70,94 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    if not game_over:
-        # Update the bird's position in the vertical direction
-        bird_y += bird_velocity
+    if current_state == STATE_MENU:
+        # Display the menu
+        menu_text = menu_font.render('Knife Hit Game', True, (255, 255, 255))
+        play_again_text = font.render('Press Enter to Play', True, (139,131,120))
+        window.blit(menu_text, (width // 2 - 180, height // 2 - 50))
+        window.blit(play_again_text, (width // 2 - 100, height // 2 + 50))
 
-        # Check if the bird reaches the screen's boundaries and reverse direction if needed
-        if bird_y <= 0:
-            bird_y = 0
-            bird_velocity = abs(bird_velocity)  # Change direction to down
-        elif bird_y >= height - bird_height:
-            bird_y = height - bird_height
-            bird_velocity = -abs(bird_velocity)  # Change direction to up
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:  # Enter key
+            current_state = STATE_PLAYING
 
-        # Handle knife movement based on player input (tap)
-        if knife_shot:
-            if knife_x > 0:
-                knife_x -= knife_velocity
-            else:
-                knife_shot = False  # Reset the knife's position
+    elif current_state == STATE_PLAYING:
+        if not game_over:
+            # Update the bird's position in the vertical direction
+            bird_y += bird_velocity
+
+            # Check if the bird reaches the screen's boundaries and reverse direction if needed
+            if bird_y <= 0:
+                bird_y = 0
+                bird_velocity = abs(bird_velocity)  # Change direction to down
+            elif bird_y >= height - bird_height:
+                bird_y = height - bird_height
+                bird_velocity = -abs(bird_velocity)  # Change direction to up
+
+            # Handle knife movement based on player input (tap)
+            if knife_shot:
+                if knife_x > 0:
+                    knife_x -= knife_velocity
+                else:
+                    knife_shot = False  # Reset the knife's position
+                    knife_x = width - 50
+                    knife_y = height // 2 - knife_height // 2  # Position in the center vertically
+
+            # Check if the screen was tapped (left mouse button)
+            if pygame.mouse.get_pressed()[0]:  # 0 represents the left mouse button
+                if not knife_shot and chances > 0:
+                    knife_shot = True
+                    knife_x = width - 50
+                    knife_y = height // 2 - knife_height // 2  # Position in the center vertically
+                    chances -= 1
+
+            # Check for collision between knife and bird
+            if knife_shot and knife_x < bird_x + bird_width and knife_x + knife_width > bird_x and knife_y < bird_y + bird_height and knife_y + knife_height > bird_y:
+                # Collision occurred, increment the score and reset the knife
+                score += 1
+                knife_shot = False
                 knife_x = width - 50
-                knife_y = height // 2 - knife_height // 2  # Position in the center vertically
+                knife_y = height // 2 - knife_height // 2
 
-        # Check if the screen was tapped (left mouse button)
-        if pygame.mouse.get_pressed()[0]:  # 0 represents the left mouse button
-            if not knife_shot and chances > 0:
-                knife_shot = True
-                knife_x = width - 50
-                knife_y = height // 2 - knife_height // 2  # Position in the center vertically
-                chances -= 1
+            # Check for "Game Over" condition
+            if chances == 0:
+                game_over = True
 
-        # Check for collision between knife and bird
-        if knife_shot and knife_x < bird_x + bird_width and knife_x + knife_width > bird_x and knife_y < bird_y + bird_height and knife_y + knife_height > bird_y:
-            # Collision occurred, increment the score and reset the knife
-            score += 1
-            knife_shot = False
-            knife_x = width - 50
-            knife_y = height // 2 - knife_height // 2
+        # Draw the background image on the window
+        window.blit(background, (0, 0))
 
-        # Check for "Game Over" condition
-        if chances == 0:
-            game_over = True
+        # Draw the bird at the left side
+        window.blit(bird, (bird_x, bird_y))
 
-    # Draw the background image on the window
-    window.blit(background, (0, 0))
+        # Draw the knife on the right side in the center
+        window.blit(knife, (knife_x, knife_y))
 
-    # Draw the bird at the left side
-    window.blit(bird, (bird_x, bird_y))
+        # Display the score on the window
+        score_text = font.render(f'Score: {score}', True, (0,0,0))
+        window.blit(score_text, (10, 10))
 
-    # Draw the knife on the right side in the center
-    window.blit(knife, (knife_x, knife_y))
+        # Display the remaining chances on the window
+        chances_text = font.render(f'Chances: {chances}', True, (0,0,0,))
+        window.blit(chances_text, (10, 50))
 
-    # Display the score on the window
-    score_text = font.render(f'Score: {score}', True, (255, 255, 255))
-    window.blit(score_text, (10, 10))
+        if game_over:
+            current_state = STATE_GAME_OVER
 
-    # Display the remaining chances on the window
-    chances_text = font.render(f'Chances: {chances}', True, (255, 255, 255))
-    window.blit(chances_text, (10, 50))
+    elif current_state == STATE_GAME_OVER:
+        # Display the game over menu
+        game_over_text = menu_font.render('Game Over', True, (255, 0, 0))
+        play_again_text = font.render('Press Enter to Play Again', True, (255, 255, 255))
+        window.blit(game_over_text, (width // 2 - 130, height // 2 - 50))
+        window.blit(play_again_text, (width // 2 - 100, height // 2 + 50))
 
-    # Display "Game Over" when game over
-    if game_over:
-        game_over_text = font.render('Game Over', True, (255, 0, 0))
-        window.blit(game_over_text, (width // 2 - 80, height // 2 - 20))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RETURN]:  # Enter key
+            current_state = STATE_PLAYING
+            # Reset the game state
+            bird_y = height // 2
+            score = 0
+            chances = 4
+            game_over = False
 
     pygame.display.update()
 
